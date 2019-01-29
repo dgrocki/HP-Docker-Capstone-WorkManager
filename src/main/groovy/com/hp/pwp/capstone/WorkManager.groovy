46d50b202575021f15662e501766f29769b1cec6
grocki <grocki@localhost.localdomain>
2019-01-28 20:44:58 -0800

    added tubes not working properly onjob recieve

--- src/main/groovy/com/hp/pwp/capstone/WorkManager.groovy
+++ src/main/groovy/com/hp/pwp/capstone/WorkManager.groovy
@@ -2,42 +2,68 @@ package com.hp.pwp.capstone
 import com.surftools.BeanstalkClientImpl.ClientImpl
 import com.surftools.BeanstalkClientImpl.JobImpl
 import com.surftools.BeanstalkClient.BeanstalkException
-
-
 class WorkManager{
-	private ClientImpl connection = new ClientImpl("0.0.0.0", 11300);
-	private JobImpl currentJob;	//can we only be working on one job at a time?
-
-	public void sendWork(String json){
-		long priority = 0;
-		int delaySeconds = 0;
-		int timeToRun = 10;
-		byte[] data = json.getBytes();
+	private BeanstalkClient beanstalk = new BeanstalkClient();		
+	//pull a new job off the new_work queue
+	public recieve_new_work(){
+		beanstalk.useTube("new_work");
 		
-		connection.put(priority, delaySeconds, timeToRun, data);
+		//?????
+		JobImpl job = beanstalk.recieveWork();
 		
-	}	
-	public JobImpl recieveWork(){
-		currentJob = connection.reserve();
-		return currentJob;
-	}
-	public void deleteWork(long id){	//when should we delete the job off of the queue?
-		connection.delete(id);
-		
-	}
-	public static void main(String [] args) {
-		WorkManager wm = new WorkManager();		
-
-		String input = "Hello World";	
-		wm.sendWork(input);
-		JobImpl job = wm.recieveWork();
-		
-		println "Byte array comming back:\n"+ job.data;
+		println "here";	
 		String s = new String(job.data);
-		println "Byte array converted back to string:\n" + s;
+		println s;
 		
-		wm.deleteWork(job.jobId);
+		beanstalk.deleteWork(job.jobId);
+			
+		return;
+	}
+	//put a new job on the to_workerB queue
+	public send_to_workerB(){}
+	//pull a job off of the riak queue
+	public send_to_riak(){
+		beanstalk.useTube("riak");
+		JobImpl job = beanstalk.recieveWork();
+		
+		String s = new String(job.data);
+		println s;
+		
+		beanstalk.deleteWork(job.jobId);
+			
+		return;
 
+	}
+	//put a job on the status queue
+	public send_status(){}
+
+
+	public static void main(String [] args) {
+		WorkManager wm = new WorkManager();
+
+		//for testing only, only need a beanstalk client in the class
+		BeanstalkClient beanstalk = new BeanstalkClient();		
+	
+		beanstalk.useTube("new_work");
+		String input = "New work from the outside world";	
+		beanstalk.sendWork(input);
+		beanstalk.useTube("new_work");
+		input = "New work number 2 from the outside world";	
+		beanstalk.sendWork(input);
+		beanstalk.useTube("riak");
+		input = "More stuff for Riak";	
+		beanstalk.sendWork(input);
+		beanstalk.useTube("riak");
+		input = "Time to put something in Riak from Worker B";	
+		beanstalk.sendWork(input);
+		
+		
+		wm.recieve_new_work();
+		println "here";	
+		wm.send_to_riak();
+		println "here";	
+		wm.recieve_new_work();
+		wm.send_to_riak();
 		return;
 	}
 
