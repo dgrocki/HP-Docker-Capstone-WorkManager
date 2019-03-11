bd1b1efa06d16ae6a1f36286d79cb6f0f6b197b5
sanderau <sanderau@oregonstate.edu>
2019-03-11 13:51:24 -0700

    GetWork now interprets the work as a JSON object

--- src/main/groovy/com/hp/pwp/capstone/GetWork.groovy
+++ src/main/groovy/com/hp/pwp/capstone/GetWork.groovy
@@ -7,6 +7,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import groovy.json.JsonSlurper
 
 @SuppressWarnings("serial")
 public class GetWork extends HttpServlet 
@@ -24,7 +25,14 @@ public class GetWork extends HttpServlet
     protected void doGet(HttpServletRequest request, HttpServletResponse response) 
     throws ServletException, IOException 
     {
+        def parser = new JsonSlurper()
+
         output = servBeanStalk.recieve_new_work();
+
+        def json = parser.parseText(output)
+
+        println( json )
+
         try {
             response.setContentType("text/html;charset=UTF-8");
             PrintWriter out = response.getWriter();
